eb574844e73cbd308428e747aca15271b13f26ce
preficient <alexandraruizfernandez22@gmail.com>
2023-01-02 22:22:58 -0500

    Pre 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -2,8 +2,54 @@ using System;
 
 class Program
 {
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter;
+        
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int percentage = int.Parse(userInput);
+    
+
+        if (percentage >= 90)
+        {   
+            letter = "A";
+            
+        }
+        else if (percentage >= 80 && percentage < 90)
+        {
+            letter = "B";
+            
+        }
+        else if (percentage >= 70 && percentage < 80)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60 && percentage < 70)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+
+
+
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations!! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("You did not passed the course. You can do it better next time. Just hang in there!");
+        }
+        
     }
+
+    
 }
