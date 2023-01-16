29f419eb8cf8d2fe7d2911bb829661bf1185b615
preficient <alexandraruizfernandez22@gmail.com>
2023-01-16 14:48:35 -0500

    learning 3

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,48 @@
+public class Fraction
+{
+    public int _top;
+    public int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+    public Fraction(int wholenumber)
+    {
+        _top = wholenumber;
+        _bottom = 1;
+    }
+    public Fraction (int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+
+    }
+    public int GetTop()
+    {
+        return _top;
+    }
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        string text = $"{_top}/{_bottom}";
+        return text;
+    }
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double)_bottom;
+    }
+}
